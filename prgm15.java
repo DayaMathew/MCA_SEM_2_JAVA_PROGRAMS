e006217b0aff86251815e9d4cffd4c2e9a33fa87
DayaMathew <dayamathew29@gmail.com>
2026-01-29 23:53:33 +0530

    Add files via upload

---
+++ prgm15.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class prgm15{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        if (n < 2) {
+            System.out.println("Array must have at least 2 elements");
+            return;
+        }
+
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < smallest) {
+                secondSmallest = smallest;
+                smallest = arr[i];
+            } else if (arr[i] > smallest && arr[i] < secondSmallest) {
+                secondSmallest = arr[i];
+            }
+        }
+
+        System.out.println("Second smallest element = " + secondSmallest);
+    }
+}
