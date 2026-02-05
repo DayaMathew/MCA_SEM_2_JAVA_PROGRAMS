e222658c92683cdf1911466d78ec01bd03951250
DayaMathew <dayamathew29@gmail.com>
2026-02-06 05:18:37 +0530

    Add files via upload

--- prgm7.java
+++ prgm7.java
@@ -1,30 +1,17 @@
 import java.util.Scanner;
 
 public class prgm7{
-
-    // Function to return maximum of two numbers
-    public static int max(int a, int b) {
-        return (a > b) ? a : b;
-    }
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter first number: ");
-        int x = sc.nextInt();
+        System.out.print("Enter three numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        System.out.print("Enter second number: ");
-        int y = sc.nextInt();
+        // Single statement to find minimum
+        int min = Math.min(a, Math.min(b, c));
 
-        System.out.print("Enter third number: ");
-        int z = sc.nextInt();
-
-        // Using max() function to get maximum of three numbers
-        int maxOfThree = max(max(x, y), z);
-
-        System.out.println("Maximum of the three numbers = " + maxOfThree);
-
-        sc.close();
+        System.out.println("Minimum = " + min);
     }
 }
