c7f5bb67f855bda833744a8dc3028c1b66c5cd3c
DayaMathew <dayamathew29@gmail.com>
2026-02-06 05:17:48 +0530

    Add files via upload

---
+++ prgm6_1.java
@@ -0,0 +1,21 @@
+public class prgm6_1{
+	public static void main(String[]args)
+	{
+		int a=10,b=25,c=15;
+		int max;
+		if(a>b&&a>c)
+		{
+			max=a;
+		}
+		else if(b>a&&b>c)
+		{
+			max=b;
+		}
+		else
+		{
+			max=c;
+		}
+	System.out.println("Maximum ="+max);
+	}
+}
+		
